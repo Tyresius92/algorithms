3e9baadd1c7efd1e9967e53d7d97b62c78f2f9d6
Tyrel <tyrelclayton92@gmail.com>
2019-07-25 19:26:23 -0400

    remove unnecessary text in bubblesort

--- sort/bubblesort/bubbleSort.js
+++ sort/bubblesort/bubbleSort.js
@@ -4,33 +4,6 @@ const swap = (input, a, b) => {
   input[b] = temp;
 };
 
-// const isSorted = input => {
-//   for (let i = 0; i < input.length - 1; i++) {
-//     if (input[i] > input[i + 1]) {
-//       return false;
-//     }
-//   }
-//
-//   return true;
-// };
-//
-// const shuffle = input => {
-//   for (let i = 0; i < input.length; i++) {
-//     const randIndex = Math.floor(Math.random() * Math.floor(i));
-//     swap(input, i, randIndex);
-//   }
-// };
-//
-// // secretly this is BOGOSORT
-// export const bubbleSort = input => {
-//   while (!isSorted(input)) {
-//     shuffle(input);
-//     console.log(input);
-//   }
-//
-//   return input;
-// };
-
 export const bubbleSort = input => {
   for (let i = 0; i < input.length - 1; i++) {
     let swapped = false;
